acfa6d127dacbb3b5214d788738bb8da530227e3
aissaiyahMacVersion2 <aissaiyah.bennett.sia@gmail.com>
2023-01-20 09:06:19 -0500

    I did a thing
    Created scene where player can walk onto a set empty object called border which will contain the camera. The player can move into the border changing the camera operation and get it back when it leaves the area. Camera tracks player on the x and y axis

--- Assets/CameraScript.cs
+++ Assets/CameraScript.cs
@@ -4,16 +4,50 @@ using UnityEngine;
 
 public class CameraScript : MonoBehaviour
 {
+    public Transform player; // get object(player)
     public Camera cam;
+    public bool onBorder;
+    public Transform Border; // get empty object(border)
+    public Vector3 offset;
     // Start is called before the first frame update
     void Start()
     {
         cam = Camera.main;
+        onBorder = false;// determines if camera is on empty game object border
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(player.position.x <= -20)// if player x position is less than -20 set camera to border
+        {
+            onBorder = true;
+        }
+        if (player.position.x >= -30)// if player x position is greater than -30 set camera back to player
+        {
+            onBorder = false;
+        }
+
+
+
+        if (onBorder == false)
+        { 
+        transform.position = new Vector3(player.position.x, player.position.y, -10); // get player position.x and update on that position
+        }
+        if (onBorder == true)
+        {
+            transform.position = new Vector3(Border.position.x, 0, -10); // get player position.x and update on that position
+        }
+
 
     }
+  /*  public void OnTriggerEnter2D(Collider2D col)
+    {
+        if (GetComponent<Collider>().gameObject.tag == "Border")
+        {
+            onBorder = true;
+            Debug.Log("TAG");
+        }
+
+    }*/
 }
