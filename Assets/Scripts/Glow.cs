4c81eed33ea74cfa29be1714dab557dfbad195ab
aissaiyahMacVersion2 <aissaiyah.bennett.sia@gmail.com>
2023-05-25 17:22:06 -0400

    Obtained lvl 1

--- Assets/Scripts/Glow.cs
+++ Assets/Scripts/Glow.cs
@@ -93,14 +93,22 @@ public class Glow : MonoBehaviour
         if(bridge == null)
         {
             bridge = Instantiate(lightBridgePrefab, midPoint, Quaternion.identity);
-        }  
-        else
-        {
             bridge.transform.position = midPoint;
             calculatedScale = Vector2.Distance(lightCon1.position, lightCon2.position) - 2;//This calculates the distance between the two points to get the right scale
             bridge.transform.localScale = new Vector2(calculatedScale, 10f);//Adds the scale to the objects x scale
             rotation_Z = Mathf.Atan2(lightCon2.position.y - lightCon1.position.y, lightCon2.position.x - lightCon1.position.x) * Mathf.Rad2Deg;//Gets the rotation needed between the two positions(complicated math dont ever ask me how to explain got it off of pure luck.)
-            bridge.transform.rotation = Quaternion.Euler(0, 0, rotation_Z);//Adds the needed rotaton to the Z axis
+            bridge.transform.rotation = Quaternion.Euler(0, 0, rotation_Z);//Adds the needed rotaton to the Z axis\
+            lightCon1.gameObject.GetComponent<LightBridgeConnector>().isActivated = false;
+            lightCon2.gameObject.GetComponent<LightBridgeConnector>().isActivated = false;
+            lightCon1 = null;
+            lightCon2 = null;
+
+        }  
+        else
+        {
+            Destroy(bridge);
+            bridgeTimer = 0;
+            SpawnLightBridge();
         }
     }
 }
