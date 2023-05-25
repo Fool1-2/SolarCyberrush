f8e14eb99d37ea52582e8b309f0d7229958a06ef
aissaiyahMacVersion2 <aissaiyah.bennett.sia@gmail.com>
2023-05-25 18:29:48 -0400

    Finished cutscene and fix audio bug

--- Assets/useLessScriptWereShouldNotGetRidOfYet.cs
+++ Assets/useLessScriptWereShouldNotGetRidOfYet.cs
@@ -7,6 +7,7 @@ public class useLessScriptWereShouldNotGetRidOfYet : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
         OptionsMenuScript.volume = 0.5f;
     }
 
