b0b5085626860a69232a5e5b6bdd3ea42910bd71
Tarin <tfung101@gmail.com>
2023-06-10 09:59:45 -0400

    buildmanagescript forgor save

--- Assets/InsideBuildingManagerScript.cs
+++ Assets/InsideBuildingManagerScript.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InsideBuildingManagerScript : MonoBehaviour
+public class InsideBuildingManagerScript : MonoBehaviour //Add elyjahs fancy interact thing
 {
+    public static bool isInsideBuilding;
+    public Camera mainCamera;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mainCamera = Camera.current;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isInsideBuilding)
+        {
+            mainCamera.transform.position = new Vector3(500, 500);
+        }
     }
+
+    
 }
